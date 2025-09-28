962520796b2717002dd5020326e9ae507e7dd05d
Pooria Amini <pouriyaxamini@gmail.com>
2025-09-28 18:02:34 +0330

    make brand and brand-model name normalize and unique

---
+++ HiTechStore/Migrations/20250928125218_AddNormalizedColumnForBrandTables.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HiTechStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNormalizedColumnForBrandTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedName",
+                table: "BrandModel",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedName",
+                table: "Brand",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BrandModel_NormalizedName",
+                table: "BrandModel",
+                column: "NormalizedName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Brand_NormalizedName",
+                table: "Brand",
+                column: "NormalizedName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BrandModel_NormalizedName",
+                table: "BrandModel");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Brand_NormalizedName",
+                table: "Brand");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedName",
+                table: "BrandModel");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedName",
+                table: "Brand");
+        }
+    }
+}
--- HiTechStore/models/Brand.cs
+++ HiTechStore/models/Brand.cs
@@ -1,19 +1,32 @@
 using HiTechStore.Core;
 
+using Microsoft.EntityFrameworkCore;
+
 namespace HiTechStore.Models;
 
+[Index(nameof(NormalizedName), IsUnique = true)]
 public class Brand : IModel
 {
     public int BrandId { get; set; }
     public string? Name { get; set; }
+    public string? NormalizedName
+    {
+        get => Name?.ToLower();
+        set => Name?.ToLower();
+    }
     public virtual List<BrandModel>? Models { get; set; }
 }
 
-
+[Index(nameof(NormalizedName), IsUnique = true)]
 public class BrandModel : IModel
 {
     public int BrandModelId { get; set; }
     public string? Name { get; set; }
+    public string? NormalizedName
+    {
+        get => Name?.ToLower();
+        set => Name?.ToLower();
+    }
     public string? Description { get; set; }
     public virtual Brand? Brand { get; set; }
 }
