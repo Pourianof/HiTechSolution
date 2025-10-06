9a8ada029e453e1684f879573debb5be66282444
Pooria Amini <pouriyaxamini@gmail.com>
2025-10-06 16:48:39 +0330

    dummy documentation (must learn that)

--- HiTechStore/Helpers/URLFilterQuery/QueryFiltersMarkerAttribute.cs
+++ HiTechStore/Helpers/URLFilterQuery/QueryFiltersMarkerAttribute.cs
@@ -4,6 +4,17 @@ namespace HiTechStore.Helpers.URLFilterQuery;
 public class MiscQueryFiltersMarkerAttribute : Attribute
 { }
 
+/// <summary>
+/// To partitionarize the input quey string based on their prefixes which we called it <b>"namespace"</b>
+/// <details>
+/// /// For expample we define the "ct" prefix for storing all query string keys with this prefix in different Dictionary
+/// <code>
+/// [NamespacedQueryFiltersMarkerAttribute("ct")]
+/// public Dictionary<string, QueriFilterItem> CategoryProperties {get; set;}
+/// </code>
+/// So any query strings like "ct.material" or "ct.capacity" will be there.
+/// </details>
+/// </summary>
 public class NamespacedQueryFiltersMarkerAttribute : Attribute
 {
     public string Namespace { get; }
