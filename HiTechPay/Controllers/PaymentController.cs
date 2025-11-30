f0765f5adcb6f5310415a1be9f79961755305fde
Pooria Amini <pouriyaxamini@gmail.com>
2025-11-30 23:03:13 +0330

    implement controller for creating proper callback url by signed key

--- HiTechPay/Controllers/PaymentController.cs
+++ HiTechPay/Controllers/PaymentController.cs
@@ -1,11 +1,37 @@
+using System.Web;
+
+using HiTechPay.Services;
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace HiTechPay.Controllers
 {
-    public class PaymentController : Controller
+    public class PaymentController(ISignerService signerService, IConfiguration configuration) : Controller
     {
+        private IConfiguration _configuration = configuration;
+        private ISignerService _signer = signerService;
+
         public IActionResult Index()
         {
+            var specifiedCallbackURL = HttpContext.Request.Query["callback"];
+            var key = HttpContext.Request.Query["key"];
+
+            if (!string.IsNullOrEmpty(key) && !string.IsNullOrEmpty(specifiedCallbackURL))
+            {
+                var signedKey = _signer.Sign(key!);
+                var callbackUrl = new UriBuilder(specifiedCallbackURL!);
+
+                var query = HttpUtility.ParseQueryString(callbackUrl.Query);
+
+                query["confirm_key"] = signedKey;
+
+                callbackUrl.Query = query.ToString();
+                string finalCallbackUrl = callbackUrl.ToString();
+
+                ViewBag.CallbackURL = finalCallbackUrl;
+
+            }
+
             return View();
         }
     }
