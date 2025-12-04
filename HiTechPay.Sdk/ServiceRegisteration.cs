3cc0ac0d4051733df5e02ad874922a73f8916564
Pooria Amini <pouriyaxamini@gmail.com>
2025-12-05 02:11:07 +0330

    register forgotten dependency

--- HiTechPay.Sdk/ServiceRegisteration.cs
+++ HiTechPay.Sdk/ServiceRegisteration.cs
@@ -1,4 +1,5 @@
 using HiTechPay.Sdk;
+using HiTechPay.Sdk.Communication;
 using HiTechPay.Sdk.Keys;
 
 using Microsoft.Extensions.DependencyInjection;
@@ -8,6 +9,7 @@ public static class ServiceRegistration
     public static IServiceCollection UseHiTechPaySdk(this IServiceCollection services)
     {
         services.AddScoped<IVerifier, Verifier>();
+        services.AddScoped<IServerConnectionHelper, ServerConnectionHelper>();
         services.AddScoped<IHiTechPaySdkFacade, HiTechPaySdkFacade>();
 
         return services;
