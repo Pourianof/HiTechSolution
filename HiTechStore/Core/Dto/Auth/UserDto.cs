828f8dca2d23866108cdd41e96281cab7e58408b
Pooria Amini <pouriyaxamini@gmail.com>
2026-06-26 01:51:41 +0330

    including user id in dto

--- HiTechStore/Core/Dto/Auth/UserDto.cs
+++ HiTechStore/Core/Dto/Auth/UserDto.cs
@@ -6,6 +6,7 @@ namespace HiTechStore.Core.Dto.Auth;
 [MapFrom<User>]
 public class UserDto
 {
+    public string? Id { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? Email { get; set; }
