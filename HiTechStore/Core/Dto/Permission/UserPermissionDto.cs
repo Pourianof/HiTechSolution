d98dcca43df52f169eeadda8fd3eb0fe1f2dcca4
Pooria Amini <pouriyaxamini@gmail.com>
2026-07-03 23:04:38 +0330

    fix: add map configuration

--- HiTechStore/Core/Dto/Permission/UserPermissionDto.cs
+++ HiTechStore/Core/Dto/Permission/UserPermissionDto.cs
@@ -1,9 +1,12 @@
 using HiTechStore.Core.Models;
+using HiTechStore.Helpers.AutoMapper;
 
 namespace HiTechStore.Core.Dto.Permission;
 
+[MapFrom<UserPermission>]
 public class UserPermissionDto
 {
+    [MapFromProperty(path: [nameof(UserPermission.Permission), nameof(UserPermission.Permission.Code)])]
     required public string Code { get; set; }
     public PermissionScope? Scope { get; set; } = default;
 }
