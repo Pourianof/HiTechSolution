4dfa4ee215c8493e2720525c538c0e6794e86aff
Pooria Amini <pouriyaxamini@gmail.com>
2026-07-03 16:32:26 +0330

    fix: initialize Scope property in UserPermission class

--- HiTechStore/Core/Models/UserPermission.cs
+++ HiTechStore/Core/Models/UserPermission.cs
@@ -8,7 +8,7 @@ public class UserPermission
     virtual public Permission? Permission { get; set; }
     public string? GrantedByUserId { get; set; }
     public DateTime GrantedAt { get; set; }
-    public PermissionScope Scope { get; set; }
+    public PermissionScope Scope { get; set; } = PermissionScope.Self;
 }
 
 public enum PermissionScope
